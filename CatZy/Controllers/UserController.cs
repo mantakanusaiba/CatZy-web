4f28e99ce5f0309c1207e0731c63832d3d65acef
Mahia Rahman <140752725+mahiOSD@users.noreply.github.com>
2025-09-07 16:11:42 +0000

    Doctor dashboard & Appointment

--- CatZy/Controllers/UserController.cs
+++ CatZy/Controllers/UserController.cs
@@ -1,9 +1,36 @@
-﻿using System.Web.Mvc;
+﻿using Catzy.Models;
+using System.Linq;
+using System.Web.Mvc;
 
 namespace Catzy.Controllers
 {
     public class UserController : Controller
     {
+        private AppDbContext db = new AppDbContext();
+
+        [HttpGet]
+        public ActionResult Appointment()
+        {
+            if (Session["Role"] == null || Session["Role"].ToString() != "User")
+                return RedirectToAction("Login", "Account");
+
+            return View(new Appointment());
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Appointment(Appointment model)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Appointments.Add(model);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(model);
+        }
+
         public ActionResult Index()
         {
             if (Session["Role"] == null || Session["Role"].ToString() != "User")
@@ -13,4 +40,5 @@ namespace Catzy.Controllers
             return View();
         }
     }
+        
 }
